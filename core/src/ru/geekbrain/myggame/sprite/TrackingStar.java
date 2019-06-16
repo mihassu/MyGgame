9c47fee05fbf9741e39a07d2f3b58aaf2d771e2a
mihassu <mihassu1@gmail.com>
2019-06-16 19:19:13 +0300

    Урок 8

--- core/src/ru/geekbrain/myggame/sprite/TrackingStar.java
+++ core/src/ru/geekbrain/myggame/sprite/TrackingStar.java
@@ -15,6 +15,10 @@ public class TrackingStar extends Star {
 
     @Override
     public void update(float delta) {
+        //считаем новую сорость звезд (sumV): обнуляем, добавляем скорость крорабля и умножаем
+        // на скаляр 0.2f(т.е. уменьшаем на 20%), поворачиваем на 180 град (т.о. добавляется
+        // боковое движение звездам), добавляем скорость
+        // по оси y чтобы звезды летели вниз тоже
         sumV.setZero().mulAdd(trackigV, 0.2f).rotate(180f).add(v);
         pos.mulAdd(sumV, delta);
         checkBounds();
